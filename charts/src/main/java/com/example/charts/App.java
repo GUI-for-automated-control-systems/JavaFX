14e01ac40df6c2b3d53c4ae89e8df1c33915a956
Zorin Egor <zorin-ep@mail.ru>
2023-11-17 22:58:43 +0800

    add dumy data ut realtime (ᵔ ᵕ ᵔ)

--- charts/src/main/java/com/example/charts/App.java
+++ charts/src/main/java/com/example/charts/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Application.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Application");
+        stage.setTitle("Exploring the possibilities");
         stage.setScene(scene);
         stage.show();
     }
--- charts/src/main/java/com/example/charts/AppController.java
+++ charts/src/main/java/com/example/charts/AppController.java
@@ -1,24 +1,29 @@
 package com.example.charts;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
 
 import java.sql.*;
 
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AppController {
+    private AtomicInteger tick = new AtomicInteger(0);
     @FXML
     private ResourceBundle resources;
 
@@ -27,6 +32,9 @@ public class AppController {
 
     @FXML
     private BarChart<String, Integer> barChart;
+
+    @FXML
+    public Text message;
 
     @FXML
     private Button bnt3;
@@ -48,7 +56,7 @@ public class AppController {
     private Pane fourthView;
 
     @FXML
-    private Pane secondView;
+    private BorderPane secondView;
 
     @FXML
     private Pane thirdView;
@@ -59,13 +67,14 @@ public class AppController {
             String url = "jdbc:mysql://localhost/student_list";
             Connection connection = null;
             Statement statement = null;
-
+            Text message = new Text();
             try{
+
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(url, "root", "");
                 statement = connection.createStatement();
 
-                String query = "SELECT FirstName, id FROM student_list;";
+                String query = "SELECT x, y FROM data;";
 
                 ResultSet resultSet = statement.executeQuery(query);
                 ArrayList<String> emp_no = new ArrayList<String>();
@@ -81,29 +90,66 @@ public class AppController {
                 resultSet.close();
 
                 CategoryAxis aAxis = new CategoryAxis();
-                aAxis.setLabel("FirstName");
+                aAxis.setLabel("x");
 
                 NumberAxis bAxis = new NumberAxis();
-                bAxis.setLabel("id");
+                bAxis.setLabel("y");
 
-                BarChart barChart = new BarChart(aAxis, bAxis);
+                LineChart chart = new LineChart(aAxis, bAxis);
+                chart.setTitle("График данных из mysql");
+
 
                 XYChart.Series dataSeries = new XYChart.Series();
-                dataSeries.setName("Emp table");
+                dataSeries.setName("Chart");
 
                 for (int i = 0; i < emp_no.size(); i++){
                     dataSeries.getData().add(new XYChart.Data<>(emp_no.get(i), birth_date.get(i)));
                 }
-                barChart.getData().add(dataSeries);
-                firstView.setCenter(barChart);
+                chart.getData().add(dataSeries);
+
+                message.setText("ok: true");
+                VBox vbox = new VBox();
+                vbox.getChildren().add(chart);
+                firstView.setCenter(vbox);
+                firstView.setBottom(message);
 
 
-            } catch (ClassNotFoundException | SQLException e) {
-                throw new RuntimeException(e);
+            } catch (ClassNotFoundException | SQLException | RuntimeException e) {
+                message.setText("ok: " + e);
+                firstView.setBottom(message);
             }
-
             firstView.toFront();
         } else if (event.getSource() == btn2){
+
+            final NumberAxis xAxis = new NumberAxis();
+            final NumberAxis yAxis = new NumberAxis();
+
+            xAxis.setAnimated(false);
+            xAxis.setLabel("Tick");
+
+            yAxis.setAnimated(false);
+            yAxis.setLabel("Value");
+
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+            series.setName("Values");
+
+            LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+            chart.setAnimated(false);
+            chart.getData().add(series);
+
+            Thread updateThread = new Thread(() -> {
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        Platform.runLater(() -> series.getData().add(new XYChart.Data<>(tick.incrementAndGet(), (int) (Math.random() * 100))));
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            updateThread.setDaemon(true);
+            updateThread.start();
+            secondView.setCenter(chart);
             secondView.toFront();
         } else if (event.getSource() == bnt3){
             thirdView.toFront();
